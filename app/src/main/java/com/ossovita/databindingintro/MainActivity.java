92cad69cab396c51a1edaee54330a1ea1859aab8
sancaryum <yasineryigit737@gmail.com>
2021-03-07 04:31:22 +0300

    ImageView-TextView data binding parts are done

--- app/src/main/java/com/ossovita/databindingintro/MainActivity.java
+++ app/src/main/java/com/ossovita/databindingintro/MainActivity.java
@@ -1,14 +1,30 @@
 package com.ossovita.databindingintro;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.ossovita.databindingintro.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+       ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main);
+
+       User user = new User("Yours truly",16,true,"https://upload.wikimedia.org/wikipedia/commons/b/be/Alan_Cox.jpg");
+       activityMainBinding.setUser(user);
+
+       activityMainBinding.button.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View view) {
+               Toast.makeText(MainActivity.this, user.getName(), Toast.LENGTH_SHORT).show();
+           }
+       });
     }
 }
